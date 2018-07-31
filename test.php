1d4f5049ed1fa8a4f9a52af18008a9f60f30b1a7
gaohui <853544904@qq.com>
2018-07-31 14:31:04 +0800

    modify content.tpl

--- test.php
+++ test.php
@@ -1,3 +1,6 @@
+<?php
+
+?>
 <!DOCTYPE HTML>
 <html lang="en-US">
 
@@ -8,9 +11,12 @@
 
 <body>
 <!-- 加载编辑器的容器 -->
-<script id="container" name="content" type="text/plain">
+<form action="server.php" method="post">
+    <script id="container" name="content" type="text/plain">
         这里写你的初始化内容
     </script>
+    <input type="submit" value="提交">
+</form>
 <!-- 配置文件 -->
 <script type="text/javascript" src="ueditor/ueditor.config.js"></script>
 <!-- 编辑器源码文件 -->
