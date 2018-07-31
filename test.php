0cefb0bad2a935fb260f62df31b5d6b7707cbad4
gaohui <853544904@qq.com>
2018-07-31 14:00:15 +0800

    add php ueditor

--- test.php
+++ test.php
@@ -1,10 +1,14 @@
-<script type="text/javascript" src="ckeditor/ckeditor.js"></script>
+<script id="container" name="content" type="text/plain">
+        这里写你的初始化内容
+</script>
+<script type="text/javascript" src="ueditor/ueditor.config.js"></script>
+<script type="text/javascript" src="ueditor/_examples/editor_api.js"></script>
+<script type="text/javascript">
+    var editor = UE.getEditor('container');
+</script>
 <?php
     echo @$_POST['content'];
 ?>
 <form action="" method="post">
-    <textarea id="TextArea1" class="ckeditor" name="content">
-        
-    </textarea>
-    <input type="submit" value="提交" />
+    <textarea id="container" style="width:630px;height:220px;"></textarea>
 </form>
